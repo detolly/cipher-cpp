5a0e185fe23b9bec0a6739459389c108f777c057
detolly <thomas@detolly.no>
2024-11-03 05:55:16 +0100

    this is insanity

--- solve.cpp
+++ solve.cpp
@@ -1,5 +1,6 @@
 #include <array>
-#include <print>
+#include <cstdio>
+// #include <print>
 
 #include <cipher/alphabet.hpp>
 #include <cipher/base64.hpp>
@@ -20,17 +21,19 @@ constexpr static const auto ciphertext = cipher::buffer(
 
 // constexpr static const auto alphabet = cipher::alphabet::create("/+9876543210zyxwvutsrqponmlkjihgfedcbaZYXWVUTSRQPONMLKJIHGFEDCBA");
 constexpr static const auto vigenere_alphabet = cipher::base64::DEFAULT_ALPHABET;
-constexpr static const auto VIGENERE_ALPHABET_SIZE = vigenere_alphabet.size();
+// constexpr static const auto vigenere_ascii_to_index = cipher::alphabet::create_ascii_to_index_array(vigenere_alphabet);
+// constexpr static const auto vigenere_decoding_table = cipher::vigenere::create_decode_table(vigenere_alphabet, vigenere_ascii_to_index);
 
 // constexpr const std::string_view key = "VGhlR2lhbnRUaGVHaWFudFRoZUdpYW50"sv; // TheGiantTheGiantTheGiant
 // constexpr const std::string_view key = "TheGiant"sv;
 
+constexpr static const auto VIGENERE_ALPHABET_SIZE = vigenere_alphabet.size();
 constexpr static auto create_rotated_alphabets()
 {
     std::array<cipher::alphabet::alphabet_t<VIGENERE_ALPHABET_SIZE>, VIGENERE_ALPHABET_SIZE> ret;
-    for(auto k = 0u; k < ret.size(); k++)
-        for(auto i = 0u; i < VIGENERE_ALPHABET_SIZE; i++)
-            ret[k][(i + k) % VIGENERE_ALPHABET_SIZE] = vigenere_alphabet[i];
+    for(auto i = 0u; i < VIGENERE_ALPHABET_SIZE; i++)
+        for(auto j = 0u; j < VIGENERE_ALPHABET_SIZE; j++)
+            ret[i][(i + j) % VIGENERE_ALPHABET_SIZE] = vigenere_alphabet[j];
     return ret;
 }
 
@@ -63,6 +66,8 @@ void rotate_vigenere(const std::span<char> key)
                                           key,
                                           decoding_tables[k],
                                           ascii_to_indexes[k]);
+                                          // vigenere_decoding_table,
+                                          // vigenere_ascii_to_index);
 
         std::println("Trying {}", cipher::buffer_to_string(vigenered));
 
@@ -82,7 +87,7 @@ void rotate_vigenere(const std::span<char> key)
     }
 }
 
-constexpr static const auto key_alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"sv;
+constexpr static const auto key_alphabet = "TheGiant"sv;
 constexpr void make_key(char* key, size_t current_index, size_t size)
 {
     for(auto i = 0u; i < key_alphabet.size(); i++) {
@@ -93,34 +98,8 @@ constexpr void make_key(char* key, size_t current_index, size_t size)
     }
 }
 
-// template<std::size_t len, std::size_t key_len>
-// constexpr static cipher::buffer_t<len - 1> encrypt_something(const char(&w)[len], const char(&k)[key_len])
-// {
-//     constexpr static auto alphabet = cipher::alphabet::create("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
-//     constexpr static auto ascii_to_index = cipher::alphabet::create_ascii_to_index_array(alphabet);
-//     constexpr static auto table = cipher::vigenere::create_table(alphabet);
-//     // constexpr static auto table = cipher::vigenere::create_decode_table(alphabet, ascii_to_index);
-//     constexpr static auto autokey = false;
-//
-//     const auto plaintext = cipher::buffer(w);
-//     const auto key = cipher::buffer(k);
-//
-//     auto ciphertext = cipher::empty_buffer<len - 1>();
-//     cipher::vigenere::vigenere<autokey>(std::span{ ciphertext },
-//                                     std::span{ plaintext },
-//                                     std::span{ key },
-//                                     table, 
-//                                     ascii_to_index);
-//     return ciphertext;
-// }
-//
-// const auto whatever = encrypt_something("DEFENDTHEEASTWALLOFTHECASTLE", "FORTIFICATION");
-// const auto whatever2 = encrypt_something("ISWXVIBJEXIGGBOCEWKBJEVIGGQS", "FORTIFICATION");
-
-int main()
+void bruteforce_key()
 {
-    // std::print("{}", cipher::buffer_to_string(whatever));
-
     char key[13]{ 0 };
     for(auto i = 0u; i < sizeof(key); i++)
         key[i] = key_alphabet[0];
@@ -130,6 +109,11 @@ int main()
         std::println("Key length: {}", i);
         make_key(key, 0, i);
     }
+}
 
+int main()
+{
+    bruteforce_key();
+    
 }
 
