987c4a94abde9da10cb14ce7ebfed0f470f289b7
detolly <thomas@detolly.no>
2024-11-04 23:31:55 +0100

    move some things

--- rotate.cpp
+++ rotate.cpp
@@ -14,8 +14,6 @@ using namespace std::string_view_literals;
 
 // constexpr static const auto vigenere_alphabet = cipher::alphabet::create("/+9876543210zyxwvutsrqponmlkjihgfedcbaZYXWVUTSRQPONMLKJIHGFEDCBA");
 constexpr static const auto vigenere_alphabet = cipher::base64::DEFAULT_ALPHABET;
-// constexpr static const auto vigenere_ascii_to_index = cipher::alphabet::create_ascii_to_index_array(vigenere_alphabet);
-// constexpr static const auto vigenere_decoding_table = cipher::vigenere::create_decode_table(vigenere_alphabet, vigenere_ascii_to_index);
 
 constexpr static const auto VIGENERE_ALPHABET_SIZE = vigenere_alphabet.size();
 constexpr static auto create_rotated_alphabets()
@@ -44,7 +42,7 @@ constexpr static auto create_decoding_tables(const auto& alphabets, const auto& 
 }
 
 template<std::size_t cipher_len, typename charT>
-void rotate_vigenere(const std::span<const char, cipher_len> ciphertext, const std::span<charT> key)
+constexpr static bool rotate_vigenere(const std::span<const char, cipher_len> ciphertext, const std::span<charT> key)
 {
     constexpr static const auto rotated_alphabets = create_rotated_alphabets();
     constexpr static const auto ascii_to_indexes = create_ascii_to_indexes(rotated_alphabets);
@@ -58,22 +56,15 @@ void rotate_vigenere(const std::span<const char, cipher_len> ciphertext, const s
                                         decoding_tables[k],
                                         ascii_to_indexes[k]);
 
-        // std::println("Trying {}", cipher::buffer_to_string(vigenered));
-
         auto plaintext = cipher::empty_buffer<cipher_len * 3 / 4>();
         cipher::base64::decode(std::span{ plaintext },
                                std::span{ vigenered });
 
-        bool printable{ true };
-        for(auto i = 0u; i < plaintext.size(); i++) {
-            printable = std::isprint(plaintext[i]);
-            if (!printable)
-                break;
-        }
-
-        if (printable) [[unlikely]]
-            std::println("PLAINTEXT: {}", cipher::to_string(plaintext));
+        if (cipher::is_print(std::span{ plaintext })) [[unlikely]]
+            return true;
     }
+
+    return false;
 }
 
 // constexpr static const auto ciphertext = cipher::ciphertext(
@@ -84,13 +75,16 @@ constexpr static const auto ciphertext = cipher::buffer(
     "dJxMOvO3dc1M82at2T6935roTqyWDgtGD/hwwRF3oHqFM5Vcw1"
     "JtINbsgWRm4o4/quEDkZ7x1B275bX3/Fo1");
 
-constexpr static const auto key_alphabet = "TheGiant"sv;
-constexpr void make_key(char* key, size_t current_index, size_t size)
+constexpr const std::string_view key = "TheGiant"sv;
+constexpr const auto keys = { key };
+
+constexpr static const auto key_alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"sv;
+static void make_key(char* key, std::size_t current_index, std::size_t size)
 {
     for(auto i = 0u; i < key_alphabet.size(); i++) {
         key[current_index] = key_alphabet[i];
-        rotate_vigenere(std::span{ ciphertext },
-                        std::span{ key, size });
+        if (rotate_vigenere(std::span{ ciphertext }, std::span{ key, size }))
+            std::println("KEY: {}", std::string_view { key, size });
         if (current_index + 1 < size)
             make_key(key, current_index + 1, size);
     }
@@ -98,33 +92,34 @@ constexpr void make_key(char* key, size_t current_index, size_t size)
 
 static void bruteforce_key()
 {
-    char key[13]{ 0 };
-    for(auto i = 0u; i < sizeof(key); i++)
-        key[i] = key_alphabet[0];
-
-    for(auto i = 1u; i < 13; i++) {
+    for(auto i = 1u; i < 7; i++) {
+        char key[13]{ 0 };
+        for(auto i = 0u; i < sizeof(key); i++)
+            key[i] = key_alphabet[0];
         std::println("Key length: {}", i);
         make_key(key, 0, i);
     }
 }
 
-constexpr const std::string_view key1 = "VGhlR2lhbnRUaGVHaWFudFRoZUdpYW50"sv; // TheGiantTheGiantTheGiant
-constexpr const std::string_view key2 = "TheGiant"sv;
-
-constexpr const auto keys = { key1, key2 };
-
-void wordlist_key()
+static void wordlist_key()
 {
-    for(const auto& key : keys)
+    for(const auto key : keys)
         rotate_vigenere(std::span{ ciphertext },
                         std::span{ key });
 }
 
-int main()
+int main(int argc, const char* argv[])
 {
-    if constexpr (false) {
-        bruteforce_key();
+    if (argc == 1) {
+        wordlist_key();
+        return 0;
     }
-    wordlist_key();
+    const auto arg1 = std::string_view { argv[1] };
+    if (arg1 == "bruteforce") {
+        bruteforce_key();
+    } else {
+        wordlist_key();
+    }
+    return 0;
 }
 
