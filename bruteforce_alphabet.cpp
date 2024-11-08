cbe355163907cf56b407a0c0ac46a36bb6b0bdaa
detolly <thomas@detolly.no>
2024-11-08 07:10:39 +0100

    restructure

--- bruteforce_alphabet.cpp
+++ bruteforce_alphabet.cpp
@@ -22,7 +22,10 @@ constexpr static void bruteforce_alphabet_vigenere(Alphabet<64>& alphabet, char*
         const auto key_char = key[(ciphertext_index) % key.size()];
         alphabet.try_alloc_char(key_char, [&](const char key_char) {
             alphabet.try_alloc_char(source_char, [&](const char source_char) {
-                const auto index = cipher::vigenere::alphabet_index<false, 64>(alphabet.ascii_to_index, source_char, key_char);
+                const auto index = cipher::vigenere::alphabet_index<false, 64>(
+                    alphabet.ascii_to_index, 
+                    source_char,
+                    key_char);
                 alphabet.template try_alloc_index<plaintext_alphabet>(
                     index, 
                     [&](const char c){
@@ -50,9 +53,6 @@ constexpr static void bruteforce_alphabet_substitution(Alphabet<64>& alphabet, c
     bruteforce_base64_alphabet<ciphertext, get_next_char, heuristic, you_win, progress_report>(alphabet, 0, plaintext, 0);
 }
 
-// constexpr static auto plaintext_alphabet = cipher::alphabet::create("/+9876543210zyxwvutsrqponmlkjihgfedcbaZYXWVUTSRQPONMLKJIHGFEDCBA");
-constexpr static auto plaintext_alphabet = cipher::alphabet::create("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/");
-
 // constexpr static const auto ciphertext = cipher::buffer(
 //     "OkEeZHnifuMdYB1IbHyAfb0g2FJzrVmfkKcSbKrpQGvhQ0/bvu76RdnGy/WtT7T3");
 constexpr static const auto ciphertext = cipher::buffer(
@@ -66,33 +66,34 @@ thread_local std::uint64_t iteration{0};
 
 static void bruteforce_alphabet()
 {
+    // constexpr static auto plaintext_alphabet = cipher::alphabet::create("/+9876543210zyxwvutsrqponmlkjihgfedcbaZYXWVUTSRQPONMLKJIHGFEDCBA");
+    constexpr static auto plaintext_alphabet = cipher::alphabet::create("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/");
+
     constexpr static auto you_win = [](const Alphabet<64>& alphabet, char* plain) {
         std::println("FOUND ALPHABET: {:64} PLAINTEXT: {}", alphabet.string_view(), std::string_view{ plain, ciphertext.size() });
     };
-
-    constexpr static auto heuristic = [](const auto plain) {
-        // constexpr static auto common_alphabet = cipher::alphabet::create("abcdefghijklmnopqrstuvwxyz");
-        // constexpr static auto common_alphabet = cipher::alphabet::create("ABCDEFGHIJKLMNOPQRTSUVWXYZabcdefghijklmnopqrstuvwxyz1234567890.,");
-        // constexpr static auto common_alphabet = cipher::alphabet::create("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ");
-        constexpr static auto common_alphabet = cipher::alphabet::create("0123456789 ");
-        return cipher::is_in_alphabet<common_alphabet>(plain);
-        // return cipher::is_print(plain);
-        // return cipher::is_common_print(plain);
-    };
-
     constexpr static auto progress_report = [](const auto& alphabet, const auto* plain){
         if (iteration++ % 10000000 == 0) 
             std::println(stderr, "PLAIN: {:64} ALPHABET: {}", std::string_view{ plain }, alphabet.string_view());
     };
 
-    // auto alphabet = Alphabet<64>::create_starting_configuration("TheGiant");
+    constexpr static auto heuristic = [](const auto plain) {
+        // constexpr static auto common_alphabet = cipher::alphabet::create("abcdefghijklmnopqrstuvwxyz");
+        // constexpr static auto common_alphabet = cipher::alphabet::create("ABCDEFGHIJKLMNOPQRTSUVWXYZabcdefghijklmnopqrstuvwxyz1234567890.,");
+        constexpr static auto common_alphabet = cipher::alphabet::create("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ");
+        // constexpr static auto common_alphabet = cipher::alphabet::create("0123456789 ");
+        return cipher::is_in_alphabet<common_alphabet>(plain);
+        // return cipher::is_print(plain);
+        // return cipher::is_common_print(plain);
+    };
+
     auto alphabet = Alphabet<64>::create_starting_configuration("");
     char plaintext[ciphertext.size()]{ 0 };
 
-    // bruteforce_alphabet_vigenere<plaintext_alphabet, ciphertext, key, heuristic, you_win, progress_report>(alphabet, plaintext);
-    bruteforce_alphabet_substitution<plaintext_alphabet, ciphertext, heuristic, you_win, progress_report>(alphabet, plaintext);
+    bruteforce_alphabet_vigenere<plaintext_alphabet, ciphertext, key, heuristic, you_win, progress_report>(alphabet, plaintext);
+    // bruteforce_alphabet_substitution<plaintext_alphabet, ciphertext, heuristic, you_win, progress_report>(alphabet, plaintext);
 
-    std::println("FINAL ALPHABET CONFIG: {} PLAINTEXT: {}", alphabet.string_view(), std::string_view{ plaintext, ciphertext.size() });
+    std::println("done?", alphabet.string_view(), std::string_view{ plaintext, ciphertext.size() });
 }
 
 int main()
