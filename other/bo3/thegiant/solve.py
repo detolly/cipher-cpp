5eca147dabfeb0ccb7cd95dc90d70c2630974464
detolly <thomas@detolly.no>
2024-11-14 07:13:31 +0100

    some notes

--- other/bo3/thegiant/solve.py
+++ other/bo3/thegiant/solve.py
@@ -1,72 +1,24 @@
 
-with open("cipher.bin", "rb") as f:
-    b = f.read()
+# with open("cipher.txt", "r") as f:
+#     c = f.read().replace('\n', '')
+#
+# l = [[c[i*4], c[i*4+1], c[i*4+2], c[i*4+3]] for i in range(len(c)//4)]
+#
+# print(l)
 
-print(bytes(b).hex())
+with open("cipher2.bin", "rb") as f:
+    c = f.read()
 
-w = list(b)
-# print(len(w))
-# print(w)
+d: dict[int, int] = {}
 
-window1 = [1,0,0,0,1,1,1,0,1,1,
-           1,1,0,0,1,0,1,0,0,1,
-           1,0,0,1,1,1,1,1,0,1,
-           1,0,0,0,1,1,0,0,1,1,
-           1,1,1,0,0,1,1,1,1,1]
+for b in c:
+    if d.__contains__(b):
+        d[b] += 1
+    else:
+        d[b] = 1
 
-window2 = [1,1,1,1,1,0,0,1,1,1,
-           1,1,0,0,1,1,0,0,0,1,
-           1,0,1,1,1,1,1,0,0,1,
-           1,0,0,1,0,1,0,0,1,1,
-           1,1,0,1,1,1,0,0,0,1]
 
-window3 = [1,1,1,1,1,1,0,0,0,1,
-           1,0,0,0,1,1,0,0,1,1,
-           0,0,0,1,1,1,1,1,1,1,
-           1,0,0,0,0,0,0,0,0,1,
-           1,1,0,0,0,0,0,0,1,1]
+for k in d.keys():
+    print("{} {}".format(k, d[k]))
 
-window4 = [1,0,0,0,0,0,0,1,1,1,
-           1,1,0,0,1,1,0,0,0,1,
-           1,1,1,1,1,1,1,1,1,1,
-           1,0,1,1,0,1,0,0,0,1,
-           1,1,0,1,1,1,0,0,1,1]
-
-window5 = [1,1,1,1,1,1,1,1,1,1,
-           1,1,0,0,1,1,1,0,1,1,
-           1,1,1,1,1,1,1,1,1,1,
-           1,0,1,1,1,1,0,0,1,1,
-           1,1,1,1,1,1,1,1,1,1]
-
-def make_bytes_from_window(window) -> bytearray:
-    arr = []
-    currentbyte = 0
-    for i in range(len(window)):
-        if i % 8 == 0 and i != 0:
-            arr.append(currentbyte)
-            currentbyte = 0
-        currentbyte = (currentbyte << 1) | window[i]
-    if currentbyte != 0:
-        lastbyte = 0
-        for i in range(len(window) % 8): lastbyte |= (1 << 7 - i) 
-        arr.append(lastbyte)
-    return bytearray(arr)
-
-allwindows = window1 + window2 + window3 + window4 + window5
-print(len(allwindows))
-arr = make_bytes_from_window(allwindows)
-
-print(list(arr))
-print(arr.hex())
-
-for i in range(len(arr)):
-    arr[i] = 0b11111111 - arr[i]
-print(list(arr))
-print(arr.hex())
-print(len(list(arr)))
-
-for i in range(12):
-    for j in range(12):
-        print("{:2x} ".format(w[i*12+j]), end="")
-    print()
-
+#print(d)
