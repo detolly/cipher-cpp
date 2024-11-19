4219a74915da692fd0627654d95a2f4cee9b0989
detolly <thomas@detolly.no>
2024-11-19 16:58:30 +0100

    Add column transposition tool

---
+++ cipher/transposition.hpp
@@ -0,0 +1,42 @@
+#include <algorithm>
+#include <vector>
+#include <print>
+
+#include <cipher/alphabet.hpp>
+
+namespace cipher::transposition
+{
+
+template<bool encode = true,
+         typename charT1, typename charT2, typename charT3,
+         std::size_t ex1, std::size_t ex2, std::size_t ex3>
+constexpr static void column(const std::span<charT1, ex1> target,
+                             const std::span<charT2, ex2> source,
+                             const std::span<charT3, ex3> key,
+                             const cipher::alphabet::ascii_to_index_t& ascii_to_index)
+{
+    std::vector<std::tuple<std::uint8_t, std::uint8_t>> sorted_keys;
+    sorted_keys.reserve(key.size());
+    for(auto i = 0u; i < key.size(); i++)
+        sorted_keys.push_back({ i, ascii_to_index[static_cast<std::uint8_t>(key[i])] });
+
+    std::sort(sorted_keys.begin(), sorted_keys.end(), [](const auto& a, const auto& b){
+        return std::get<1>(b) > std::get<1>(a);
+    });
+
+    const auto num_rows = (target.size() / key.size()) + 1;
+    for(auto i = 0u; i < key.size(); i++) {
+        for(auto row = 0u; row < num_rows; row++) {
+            if (i * key.size() + i >= target.size())
+                break;
+            const auto column_index = std::get<0>(sorted_keys[i]);
+            if constexpr (encode) {
+                target[row * key.size() + i] = source[row * key.size() + column_index];
+            } else {
+                target[row * key.size() + column_index] = source[row * key.size() + i];
+            }
+        }
+    }
+}
+
+}
--- tools/column_transposition.cpp
+++ tools/column_transposition.cpp
@@ -6,17 +6,16 @@
 
 #include <cipher/alphabet.hpp>
 #include <cipher/base64.hpp>
-#include <cipher/substitution.hpp>
+#include <cipher/transposition.hpp>
 
 int main(int argc, const char* argv[])
 {
-    argparse::ArgumentParser parser("substitution");
+    argparse::ArgumentParser parser("transposition");
     
-    parser.add_argument("-p", "--plaintext-alphabet")
+    parser.add_argument("-a", "--alphabet")
         .default_value(std::string(cipher::base64::DEFAULT_ALPHABET.begin(), cipher::base64::DEFAULT_ALPHABET.size()));
-    parser.add_argument("-c", "--ciphertext-alphabet")
-        .required();
     parser.add_argument("-d", "--decode").flag().default_value(false);
+    parser.add_argument("-k", "--key").required();
     parser.add_argument("--debug").flag().default_value(false);
     parser.add_argument("source").required();
 
@@ -47,23 +46,27 @@ int main(int argc, const char* argv[])
     if (debug)
         std::println(stderr, "DECODE: _{}_", decode);
 
-    const auto plaintext_alphabet = parser.get<std::string>("--plaintext-alphabet");
+    const auto alphabet = parser.get<std::string>("--alphabet");
     if (debug)
-        std::println(stderr, "PLAINTEXT_ALPHABET: _{}_", plaintext_alphabet);
+        std::println(stderr, "ALPHABET: _{}_", alphabet);
 
-    const auto ciphertext_alphabet = parser.get<std::string>("--ciphertext-alphabet");
+    const auto key = parser.get<std::string>("--key");
     if (debug)
-        std::println(stderr, "CIPHERTEXT_ALPHABET: _{}_", ciphertext_alphabet);
+        std::println(stderr, "KEY: _{}_", key);
 
-    const auto source_ascii_to_index = cipher::alphabet::create_ascii_to_index_array(decode ? std::span{ ciphertext_alphabet } : std::span{ plaintext_alphabet });
-    const auto target_alphabet = decode ? std::span{ plaintext_alphabet } : std::span{ ciphertext_alphabet };
-    if (debug)
-        std::println(stderr, "TARGET_ALPHABET: _{}_", std::string_view{ target_alphabet });
+    const auto source_ascii_to_index = cipher::alphabet::create_ascii_to_index_array(std::span{ alphabet });
 
-    cipher::substitution::substitute(std::span{ target },
-                                     std::span{ source },
-                                     source_ascii_to_index,
-                                     target_alphabet);
+    if (decode) {
+        cipher::transposition::column<false>(std::span{ target },
+                                             std::span{ source },
+                                             std::span{ key },
+                                             source_ascii_to_index); 
+    } else {
+        cipher::transposition::column<true>(std::span{ target },
+                                            std::span{ source },
+                                            std::span{ key },
+                                            source_ascii_to_index); 
+    }
 
     std::println("{}", target);
 }
