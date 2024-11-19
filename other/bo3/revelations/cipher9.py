bb0f49b1cec96ad52bb34e5a911fb2078b7593fe
detolly <thomas@detolly.no>
2024-11-19 11:33:07 +0100

    Notes

--- other/bo3/revelations/cipher9.py
+++ other/bo3/revelations/cipher9.py
@@ -3,20 +3,26 @@ with open('cipher9.bin', 'rb') as f:
     b = f.read()
 
 res = ""
+res2 = ""
 
+n = 0
 for i in range(len(b)):
     for k in range(8):
         bit = (1 << (7 - k))
         if (b[i] & bit):
-            res += "\N{ESC}[31m1"
-            # res += "-"
+            res2 += "\N{ESC}[31m1"
+            res += "1"
         else:
-            res += "\N{ESC}[32m0"
-            # res += "."
+            res2 += "\N{ESC}[32m0"
+            res += "0"
+        n+=1
+        # if n % 2 == 0:
+        #     res2+=" "
     if (i+1) % 8 == 0:
-        res += "\n"
+        res2+="\n"
 
-print(res)
+print(res.replace("0000000", "/").replace("000", " ").replace("1110", "-").replace("10", "."))#.replace("0", ""))
+print(res2)
 
 # # print(res)
 # n = 8
