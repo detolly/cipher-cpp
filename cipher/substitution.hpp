bc9bc4edf852a0e40cac757a2c28fb826604217a
detolly <thomas@detolly.no>
2024-11-19 11:17:36 +0100

    Tools: Add substitution tool

--- cipher/substitution.hpp
+++ cipher/substitution.hpp
@@ -7,17 +7,14 @@
 namespace cipher::substitution
 {
 
-template<std::size_t ALPHABET_LENGTH, typename charT, typename charT2, std::size_t ex1, std::size_t ex2>
+template<typename charT, typename charT2, typename charT3, std::size_t ex1, std::size_t ex2, std::size_t ex3>
 constexpr static void substitute(const std::span<charT, ex1> target,
-                             const std::span<charT2, ex2> source,
-                             const alphabet::ascii_to_index_t<ALPHABET_LENGTH>& source_ascii_to_index,
-                             const alphabet::alphabet_t<ALPHABET_LENGTH>& target_alphabet)
+                                 const std::span<charT2, ex2> source,
+                                 const alphabet::ascii_to_index_t& source_ascii_to_index,
+                                 const std::span<charT3, ex3>& target_alphabet)
 {
-    if consteval {
-        static_assert(source.size() == target.size());
-    }
     for(auto i = 0u; i < target.size(); i++) {
-        target[i] = target_alphabet[source_ascii_to_index[source[i]]];
+        target[i] = target_alphabet[source_ascii_to_index[static_cast<std::uint8_t>(source[i])]];
     }
 }
 
---
+++ tools/substitution.cpp
@@ -0,0 +1,61 @@
+
+#include "cipher/alphabet.hpp"
+#include <print>
+
+#include <argparse.hpp>
+
+#include <cipher/substitution.hpp>
+#include <cipher/base64.hpp>
+
+int main(int argc, const char* argv[])
+{
+    argparse::ArgumentParser parser("substitution");
+    
+    parser.add_argument("-p", "--plaintext-alphabet")
+        .default_value(std::string(cipher::base64::DEFAULT_ALPHABET.begin(), cipher::base64::DEFAULT_ALPHABET.size()));
+    parser.add_argument("-c", "--ciphertext-alphabet")
+        .required();
+    parser.add_argument("-d", "--decode").flag().default_value(false);
+    parser.add_argument("--debug").flag().default_value(false);
+    parser.add_argument("source").required();
+
+    try {
+        parser.parse_args(argc, argv);
+    } catch(const std::exception& e) {
+        std::println(stderr, "{}", e.what());
+        std::exit(1);
+    }
+
+    const auto debug = parser.get<bool>("--debug");
+    const auto source = parser.get<std::string>("source");
+    if (debug)
+        std::println(stderr, "SOURCE: _{}_", source);
+
+    const auto decode = parser.get<bool>("--decode");
+    if (debug)
+        std::println(stderr, "DECODE: _{}_", decode);
+
+    std::string target;
+    target.resize(source.length());
+
+    const auto plaintext_alphabet = parser.get<std::string>("--plaintext-alphabet");
+    if (debug)
+        std::println(stderr, "PLAINTEXT_ALPHABET: _{}_", plaintext_alphabet);
+
+    const auto ciphertext_alphabet = parser.get<std::string>("--ciphertext-alphabet");
+    if (debug)
+        std::println(stderr, "CIPHERTEXT_ALPHABET: _{}_", ciphertext_alphabet);
+
+    const auto source_ascii_to_index = cipher::alphabet::create_ascii_to_index_array(decode ? std::span{ ciphertext_alphabet } : std::span{ plaintext_alphabet });
+    const auto target_alphabet = decode ? std::span{ plaintext_alphabet } : std::span{ ciphertext_alphabet };
+    if (debug)
+        std::println(stderr, "TARGET_ALPHABET: _{}_", std::string_view{ target_alphabet });
+
+    cipher::substitution::substitute(std::span{ target },
+                                     std::span{ source },
+                                     source_ascii_to_index,
+                                     target_alphabet);
+
+    std::println("{}", target);
+}
+
